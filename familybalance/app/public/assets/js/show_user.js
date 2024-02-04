ac36629aeb628d547445d3a7bc5bf104b14e8a11
Camilla Giaccari <cami.lla@me.com>
2024-02-05 00:18:52 +0100

    Fixed profile

--- familybalance/app/public/assets/js/show_user.js
+++ familybalance/app/public/assets/js/show_user.js
@@ -1,19 +1,11 @@
-// Useful elements
-const tableBody = document.getElementById('user_table_body');
-
-// Shows user info in its table
+// Shows user info in its card
 getUser().then(user => {
-    const tr = document.createElement("tr");
-    const username = document.createElement("td");
-    const name = document.createElement("td");
-    const surname = document.createElement("td");
+    const username = document.getElementById("username");
+    const name = document.getElementById("name");
+    const surname = document.getElementById("surname");
     username.innerText = user.username;
     name.innerText = user.name;
     surname.innerText = user.surname;
-    tableBody.appendChild(tr);
-    tr.appendChild(username);
-    tr.appendChild(name);
-    tr.appendChild(surname);
 });
 
 // Takes the user info using api
